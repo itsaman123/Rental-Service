01c09b1eb1d599c76a549c2828fcd6ca958ac086
Aman Murya <m.amanmaurya49@gmail.com>
2023-05-02 13:59:49 +0530

    added booking schema

---
+++ models/bookingSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  sdate: { 
+    type: String, 
+    required: true 
+},
+  stime: { 
+    type: Date, 
+    required: true 
+},
+  edate: { 
+    type: Date, 
+    required: true
+ },
+  etime: { 
+    type: Date, 
+    required: true 
+},
+  file: { 
+    type: String, 
+    required: true },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
